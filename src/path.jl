752bb63db6dabe5e918572e4c59589483a5bb0f8
Owen Lynch <root@owenlynch.org>
2024-08-08 15:02:24 -0700

    added some utilities for paths

--- src/path.jl
+++ src/path.jl
@@ -19,15 +19,37 @@ Base.isempty(p::Path) = isempty(p.segments)
 
 head(p::Path) = p.segments[1]
 
-tail(p::Path) = Subpath(p, 2)
+tail(p::Path) =
+    length(p) > 0 ? Subpath(p, 2) : error("tried to take the tail of an empty path")
+
+function segments(p::Path)
+    p.segments
+end
 
 struct Subpath <: AbstractPath
     path::Path
     starting_from::Int
 end
 
+function Base.length(p::Subpath)
+    length(p.path) - p.starting_from + 1
+end
+
+@tests Tuple{Subpath, length} begin
+    @test length(tail(Path([:a, :b, :c]))) == 2
+end
+
 Base.isempty(p::Subpath) = p.starting_from > length(p.path)
 
 head(p::Subpath) = p.path.segments[p.starting_from]
 
-tail(p::Subpath) = Subpath(p.path, p.starting_from + 1)
+tail(p::Subpath) =
+    length(p) > 0 ? Subpath(p.path, p.starting_from + 1) : error("tried to take the tail of an empty path")
+
+function segments(p::Subpath)
+    p.path.segments[p.starting_from:end]
+end
+
+function Base.:(*)(p1::AbstractPath, p2::AbstractPath)
+    Path([segments(p1); segments(p2)])
+end
